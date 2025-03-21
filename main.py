af4ae47604a6a995cb6cd9686b0c7685098ab5e3
Ugodlt34 <ugo.delattre@ynov.com>
2025-03-21 12:11:15 +0100

    main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from api.user_routes import user_bp
-from api.product_routes import product_bp
-from api.cart_routes import cart_bp
-from api.order_routes import order_bp
-
-app = Flask(__name__)
